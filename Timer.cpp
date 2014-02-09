b192141c0896007da5ae9f29605feb0f6b51ea72
Alex <aamartir@gmail.com>
2014-02-09 12:03:00 -0500

    Update Timer.cpp
    Added function to handle radio signal loss and initiated framework for autoand feature (Todo)

--- Timer.cpp
+++ Timer.cpp
@@ -1,19 +1,16 @@
 #include "Timer.h"
 #include "Quad.h"
+#include "ESC.h"
 #include "LEDManager.h"
 #include "Radio_PCINT.h"
 
 #include <stdio.h>
 
-uint8 debug_timer_state;
-
 Timer *Timers[] = 
 {
   &led_timer, 
   &radio_timer,
-  &speaker_timer, 
   &motor_timer,
-  &debug_timer
 };
 
 Timer::Timer()
@@ -45,7 +42,7 @@ void led_timer_handler(void)
 {
   /* Execute the current value in the pattern */
   gpio_value(led_manager.getCurrentLEDStateValue(), led_manager.getLEDpin());
-  
+      
   if(led_manager.hasNextStateInPattern() || led_manager.patternRepeats())
   {
     led_timer.setTimerVal(led_manager.getCurrentLEDStateTime());
@@ -53,7 +50,8 @@ void led_timer_handler(void)
   }
 }
 
-/* Under Construction */
+/* Handles low-level radio signal processing.
+   Higher level functions are handled within main-loop */
 void radio_timer_handler(void)
 {
   /* When radio_timer expires, we have to check 
@@ -63,43 +61,38 @@ void radio_timer_handler(void)
    * and then land.  */
   
   /* State machine */
-  switch( radio.getState() )
+  if( (radio.state & RADIO_SIGNAL_LOST) == 0 )
   {
-    case RADIO_SIGNAL_LOST:
-      /* Here, we configure autoland mode, or return to HOME 
-       * Need GPS for this. For now, just autoland. Decrease motor speeds
-       * slowly. We can use the accelerometer's z axis and integrate it
-       * to get some feedback on the speed of descent.
-       */
-      
-      break;
-      
-    default:
-      radio.setState( RADIO_SIGNAL_LOST );
+    /* Here, we configure autoland mode, or return to HOME 
+     * Need GPS for this. For now, just autoland. Decrease motor speeds
+     * slowly. We can use the accelerometer's z axis and integrate it
+     * to get some feedback on the speed of descent.
+     */ 
+     
+    gpio_set( RED_LED_PIN );
+    SET_BIT( radio.state, RADIO_SIGNAL_LOST );
   }
-  
-  /* Reset timer */
-  radio_timer.setTimerVal( RADIO_TIMER_PERIOD );
 }
 
-void debug_timer_handler(void)
-{
-  if(debug_timer_state)
-    gpio_clear(BLUE_LED_PIN);
-  else
-    gpio_set(BLUE_LED_PIN);
-  
-  debug_timer_state = !debug_timer_state;
-  //debug_timer.setTimerVal( 1 );
-}
-
-/* TODO */
-void speaker_timer_handler()
-{
-  
-}
-
+/* Do not have any altitude feedback as of now. Will have to decrease throttle slowly. */
 void motor_timer_handler()
 {
-  
+  if( quad.quad_mode == QUAD_AUTOLAND_MODE )
+  {
+    #ifdef USE_SONAR
+      // ...
+    #else
+      if( throttle > MOTOR_MIN )
+      {
+        throttle -= 5;
+        
+        /* Reset timer */
+        motor_timer.setTimerVal( MOTOR_TIMER_PERIOD );
+      }
+    #endif
+  }
+  else if( quad.quad_mode == QUAD_AUTO_TAKEOFF_MODE )
+  {
+    
+  }
 }
