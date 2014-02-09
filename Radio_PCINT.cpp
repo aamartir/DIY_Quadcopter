c39f0075e2064e7cc17f1ed31e696e2d403f834e
Alex <aamartir@gmail.com>
2014-02-09 12:04:42 -0500

    Update Radio_PCINT.cpp
    -Added mechanism to detect when radio signal is lost (See Timer.cpp). 
    -Added a really bad Autoland algorithm that is enabled when radio signal is lost. 
    -There's no altitude feedback control, which is really, really bad. Will update later.

--- Radio_PCINT.cpp
+++ Radio_PCINT.cpp
@@ -62,9 +62,10 @@ void Radio_PCINT::readReceiverData()
   /* Only extract new data if 'ready' flag is set */
   if(data_ready)
   {
-    //if( radio.radio_data[RADIO_THROTTLE_CH] >= THTLE_MIDDLE && throttle <= MOTOR_MIN )
-    //  return; 
+    /* Clear flag as soon as possible so buffer are not updated by the ISR */
+    data_ready = 0; 
     
+    /* Signal filtering */
     for( int i = 0; i < RADIO_MAX_CHANNELS; i++ )
     {
       #ifdef RADIO_SMOOTH_DATA
@@ -75,28 +76,27 @@ void Radio_PCINT::readReceiverData()
     }
     
     /* Map buffer values to actual numbers we will use in the control algorithm */
-    target[ROLL]  = map(data_rdy_buffer[RADIO_ROLL_CH],     ROLL_MIN,  ROLL_MAX,  -40, 	       40);     // ROLL
-    target[PITCH] = map(data_rdy_buffer[RADIO_PITCH_CH],    PITCH_MIN, PITCH_MAX, -40,         40);     // PITCH
+    target[ROLL]  = map(data_rdy_buffer[RADIO_ROLL_CH],     ROLL_MIN,  ROLL_MAX,  -45, 	       45);     // ROLL
+    target[PITCH] = map(data_rdy_buffer[RADIO_PITCH_CH],    PITCH_MIN, PITCH_MAX, -45,         45);     // PITCH
     throttle      = map(data_rdy_buffer[RADIO_THROTTLE_CH], THTLE_MIN, THTLE_MAX,  MOTOR_MIN,  MOTOR_MAX); // THROTTLE
     
     /* Yaw is a bit different. It will be mapped to a floating point value for more precision */
     headingIncr   = mapToDouble((double) data_rdy_buffer[RADIO_YAW_CH],    
                                 (double) YAW_MIN,   
                                 (double) YAW_MAX,    
-                                -3.0f,         
-                                 3.0f);     
+                                -10.0f,         
+                                 10.0f);     
  
     //throttle = constrain(throttle, MOTOR_MIN, MOTOR_MAX); 
     if(throttle > (MOTOR_MAX << 1) || (throttle < MOTOR_MIN))
       throttle = MOTOR_MIN;  
     
     /* Manage dead-zones (thesholds) */
-    if(abs(target[ROLL]) <= 5)
+    if(abs(target[ROLL]) <= 5.0)
        target[ROLL] = 0;
-    if(abs(target[PITCH]) <= 5)
+    if(abs(target[PITCH]) <= 5.0)
        target[PITCH] = 0;
-    if(abs(headingIncr) >= 0.5 &&
-       abs(headingIncr) <= 3.0)
+    if(abs(headingIncr) >= 0.5)
     {
       targetHeading -= headingIncr;
       
@@ -106,9 +106,6 @@ void Radio_PCINT::readReceiverData()
       //else if(targetHeading > 180)
       //  targetHeading = -180;
     }
-    
-    /* Clear flag once data is read */
-    data_ready = 0; 
   }
 }
 
@@ -116,7 +113,8 @@ volatile uint32 time_last;
 volatile uint32 time_new;
 volatile uint16 diff;
 volatile uint8 frameStarted;
-volatile uint8 last_pin_state;
+volatile uint8 pin_state;
+volatile uint8 last_pin_state = 0;
 
 /* Before you get depressed if the code/thing does NOT work, 
  * Make sure that RADIO_MAX_CHANNELS has the correct value! */
@@ -124,36 +122,47 @@ ISR( PCINT2_vect )
 {
   /* Enable interrupts for pwm control (May not need this, but leave just in case) */
   sei();
-  
+
   time_new = micros();
   diff = time_new - time_last;
   
-  //if(((PIND >> RADIO_INT_PIN) & 0x1) == 0) /* Next one will be the beginning of the frame */
-  if(!radio.data_ready && ((PIND & RADIO_PIN_MASK) != last_pin_state) ) /* Only fill buffer if data NOT used yet */
+  /* Hight or Low */
+  pin_state = (PIND >> RADIO_INT_PIN) & 0x1;
+  
+  if( !radio.data_ready ) /* Only fill buffer if data NOT used yet */
   {
-    if( (last_pin_state = (PIND & RADIO_PIN_MASK)) == 0 ) /* If signal is LOW */
+    if( pin_state == 0 && last_pin_state ) /* If signal is LOW (Beginning of new frame) */
     {
-      if(radio.chCounter == RADIO_LAST_CHAN_INDEX && 
-         radio.state & RADIO_SYNCH_END_FRAME)
+      if( radio.chCounter == RADIO_LAST_CHAN_INDEX && 
+          radio.state & RADIO_SYNCH_END_FRAME )
       {
         radio.radio_data[radio.chCounter] = time_new - radio.radio_data[radio.chCounter]; /* This is the duration of the last pulse */
         
         /* Last valid pulse? */
         if(radio.radio_data[radio.chCounter] < MIN_PW_US || radio.radio_data[radio.chCounter] > MAX_PW_US)
-          SET_BIT(radio.state, RADIO_SYNCH_ERROR);
+          SET_BIT( radio.state, RADIO_SYNCH_ERROR );
         else
         {
           radio.data_ready = 1; /* Data buffer is ready */
-          CLEAR_BIT(radio.state, RADIO_SYNCH_END_FRAME); 
+          CLEAR_BIT( radio.state, RADIO_SYNCH_END_FRAME ); 
+          
+          /* Reset radio timer */
+          if( radio.state & RADIO_SIGNAL_LOST )
+          {
+            gpio_clear( RED_LED_PIN );
+            CLEAR_BIT( radio.state, RADIO_SIGNAL_LOST ); 
+          }
           
           /* Reset radio timer */
           radio_timer.setTimerVal( RADIO_TIMER_PERIOD );
         }  
       }
-
-      //SET_BIT(radio.state, RADIO_SYNCH_START_FRAME);
-      //CLEAR_BIT(radio.state, RADIO_SYNCH_ERROR);  
-      radio.setState( RADIO_SYNCH_START_FRAME );
+      
+      /* Reset state machine */
+      SET_BIT( radio.state, RADIO_SYNCH_START_FRAME );
+      CLEAR_BIT( radio.state, RADIO_SYNCH_ERROR );
+      CLEAR_BIT( radio.state, RADIO_SYNCH_END_FRAME );
+      
       frameStarted = 0;
       radio.chCounter = 0; /* Reset counter */
     }
@@ -168,8 +177,7 @@ ISR( PCINT2_vect )
         /* Check for PW Error!!! */
         if(radio.radio_data[radio.chCounter] > MAX_PW_US)
         {
-          radio.setState( RADIO_SYNCH_ERROR );
-          //SET_BIT(radio.state, RADIO_SYNCH_ERROR);
+          SET_BIT( radio.state, RADIO_SYNCH_ERROR );
         }
         else
         {  
@@ -177,9 +185,8 @@ ISR( PCINT2_vect )
           
           if(radio.chCounter == RADIO_LAST_CHAN_INDEX)
           {
-            radio.setState( RADIO_SYNCH_END_FRAME );
-            //SET_BIT(radio.state, RADIO_SYNCH_END_FRAME);
-            //CLEAR_BIT(radio.state, RADIO_SYNCH_START_FRAME);
+            SET_BIT(radio.state, RADIO_SYNCH_END_FRAME);
+            CLEAR_BIT(radio.state, RADIO_SYNCH_START_FRAME);
           }
         }
       }
@@ -191,6 +198,7 @@ ISR( PCINT2_vect )
     }
   }
   
+  last_pin_state = pin_state;
   time_last = time_new;
 }
 
@@ -204,9 +212,5 @@ void Radio_PCINT::printRadio( void )
     Serial.print( "\t" );
   }
   
-  /* Print State */
-  Serial.print( "(" );
-  Serial.print( state );
-  Serial.println( ")" );
+  Serial.println();
 }
-
